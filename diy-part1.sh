bcdb8d7c0991161132f99ab989da12e30df46cb6
Pinkman:) <43259075+zhuang-zx2018@users.noreply.github.com>
2025-05-27 12:05:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+./scripts/feeds update UA2F
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
